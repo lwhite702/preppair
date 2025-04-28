31a4f23a8cc46a82930d9791c6efc166c5992291
lwhite702 <lee@wrelik.com>
2025-04-27 20:48:42 -0400

    Delete src/components/Header.test.tsx

--- src/components/Header.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Header from './Header';
-
-test('renders the header component', () => {
-  render(<Header />);
-  expect(screen.getByText(/header/i)).toBeInTheDocument();
-});
